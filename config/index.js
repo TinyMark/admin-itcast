9d3f6df3b631a5f62b37b63cb9ae207a557dd084
tinymark <crazybigmoon@gmail.com>
2018-09-10 17:41:58 +0800

    init

--- config/index.js
+++ config/index.js
@@ -50,8 +50,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    // assetsPublicPath: 'https://yzfh.gitee.io/admin-itcast',
+    // assetsPublicPath: '/',
+    assetsPublicPath: 'http://tinymark.github.io/admin-itcast/',
 
     /**
      * Source Maps
